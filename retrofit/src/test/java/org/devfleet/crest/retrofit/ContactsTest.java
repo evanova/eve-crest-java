a6282dcc2262f6b9f4fcbf323c69b1f5ee45ea49
evanova <evanova.mobile@gmail.com>
2016-04-17 17:27:51 -0400

    Ignore tests for now

--- retrofit/src/test/java/org/devfleet/crest/retrofit/ContactsTest.java
+++ retrofit/src/test/java/org/devfleet/crest/retrofit/ContactsTest.java
@@ -4,11 +4,13 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.devfleet.crest.model.CrestContact;
 import org.devfleet.crest.model.CrestItem;
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ContactsTest extends AbstractCrestServiceTest {
 
     @Test
+    @Ignore
     public void testContacts() {
         for (CrestContact c: service.getContacts()) {
             System.err.println(ToStringBuilder.reflectionToString(c));
@@ -16,6 +18,7 @@ public class ContactsTest extends AbstractCrestServiceTest {
     }
 
     @Test
+    @Ignore
     public void addContact() {
         final long id = Long.parseLong(property("crest.contact"));
 
--- retrofit/src/test/java/org/devfleet/crest/retrofit/FittingsTest.java
+++ retrofit/src/test/java/org/devfleet/crest/retrofit/FittingsTest.java
@@ -1,11 +1,13 @@
 package org.devfleet.crest.retrofit;
 
 import org.devfleet.crest.model.CrestFitting;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class FittingsTest extends AbstractCrestServiceTest {
 
     @Test
+    @Ignore
     public void testFittings() {
         for (CrestFitting c: service.getFittings()) {
             System.err.println(c.getName());
