b24fdbbeb99f01529d115cddecb623bd2dbbdbef
Jeremy Shore <xboxmusicn3rd@gmail.com>
2016-04-25 17:02:04 -0500

    Added MarketType and MarketOrder

---
+++ api/src/main/java/org/devfleet/crest/model/CrestMarketType.java
@@ -0,0 +1,51 @@
+package org.devfleet.crest.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by Jeremy Shore on 4/25/16.
+ */
+public class CrestMarketType extends CrestItem {
+
+    static class Type extends CrestItem {
+
+        @JsonProperty
+        private CrestItem icon;
+
+        public String getIconHref() {
+            return this.icon.getHref();
+        }
+
+    }
+
+    @JsonProperty
+    private CrestItem marketGroup;
+
+    @JsonProperty
+    private Type type;
+
+    public String getTypeName() {
+        return this.type.getName();
+    }
+
+    public String getTypeHref() {
+        return this.type.getHref();
+    }
+
+    public long getTypeId() {
+        return this.getId();
+    }
+
+    public String getGroupHref() {
+        return this.marketGroup.getHref();
+    }
+
+    public long getGroupId() {
+        return this.marketGroup.getId();
+    }
+
+    public String getTypeIcon() {
+        return this.type.getIconHref();
+    }
+
+}
